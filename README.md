385422b49cde5e4ab55a14a26601cc119f39cf88
jamesdidit72 <jamesjbarton72@gmail.com>
2021-05-26 16:05:42 +0100

    postcode it 3

--- README.md
+++ README.md
@@ -2,3 +2,47 @@
 ## Application Programming Interface
 - An API, or Application Programming Interface, is a server that you can use to retrieve and send data to using code. APIs are most commonly used to retrieve data, and that will be the focus of this beginner tutorial. When we want to receive data from an API, we need to make a request.
 - API call is what UberEats uses (FOod on its way, food delivered etc)
+
+# Post codes Exercise
+
+### Timings
+- 30 - 45 Minutes
+
+### Summary
+- Use the post codes.io to go fetch some useful data.
+- You can also create absrtaction for other APIs.
+
+### Tasks
+
+* Create a program that request a post code
+* check if the postcode is valid
+* then uses an API call to fetch more data from post code
+* display the data in user friendly format 
+  * Extra:
+  * go explore other APIs
+
+## Acceptance Criteria
+
+* uses get request
+* uses post request
+* apply OOP
+* filter response for useful information and displays
+### First iteration
+```python
+# Use python to make an API call using package called requests
+# Dependencies PIP
+import requests
+
+def check_postcode():
+    user_postcode = input('Please enter a valid postcode: ')
+    url = 'https://api.postcodes.io/postcodes/'
+    post_api_response = requests.get(url+user_postcode)
+    print(post_api_response.status_code)
+    if post_api_response.status_code == 200:
+        print(f'Thank you for your request: {post_api_response.status_code}')
+    else:
+        print('oh no! Incorrect postcode')
+check_postcode()
+```
+### Second iteration
+
--- python_API.py
+++ python_API.py
@@ -1,6 +1,28 @@
 # Use python to make an API call using package called requests
 # Dependencies PIP
 import requests
+import json
+class postcode:
+    # def __init__(self, post_api_response):
+    #     self.post_api_response = ''
+
+    def check_postcode(self):
+        user_postcode = input('Please enter a valid postcode: ')
+        url = 'https://api.postcodes.io/postcodes/'
+        post_api_response = requests.get(url+user_postcode)
+        if post_api_response.status_code == 200:
+            print(f'Thank you for your request: {post_api_response.status_code}')
+            for data in post_api_response.headers.keys():
+                print(data)
+        else:
+            print('oh no! Incorrect postcode')
+
+postcode_checker = postcode()
+postcode_checker.check_postcode()
+
+
+
+
 
 # post_api_response = requests.get("https://api.postcodes.io/postcodes/dy60lg")
 # if post_api_response.status_code == 200:
@@ -11,44 +33,20 @@ import requests
 # get the user to input postcode
 # validate postcode, before making api call
 
-def check_postcode():
-    user_postcode = input('Please enter a valid postcode: ')
-    url = 'https://api.postcodes.io/postcodes/'
-    post_api_response = requests.get(url+user_postcode)
-    print(post_api_response.status_code)
-    if post_api_response.status_code == 200:
-        print(f'Thank you for your request: {post_api_response.status_code}')
-    else:
-        print('oh no! Incorrect postcode')
-check_postcode()
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# response_bbc = requests.get('https://www.bbc.co.uk/shahrukh')  # returns 404, error
+response_bbc = requests.get('https://www.bbc.co.uk/shahrukh')  # returns 404, error
 # response_itv = requests.get('https://www.itv.com/')  # returns 200, successful
 # print(f'ITV is {response_itv}, BBC is {response_bbc}')
-
+#
 # print(response_bbc.status_code)  # shows the number in a user friendly way
 # print(response_bbc.headers)  # shows everything in the header of the url
 # print(response_bbc.content)
 # print(type(response_bbc.content))  # prints the data type (bytes)
-# data_headers = response_bbc.headers
-# for data in data_headers.values():
+# data_headers2 = response_bbc.headers
+# for data in data_headers2.values():
 #     print(data)
 # print(type(response_bbc.headers))
